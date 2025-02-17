aa982510f56d5707eece3795a9138714875bd144
Phyo-Alpha <77912443+Phyo-Alpha@users.noreply.github.com>
2025-02-17 19:18:38 +0800

    removing build

--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path, { resolve } from "path";
-import dts from "vite-plugin-dts";
+import path from "path";
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), 
-    dts({
-      tsconfigPath: './tsconfig.app.json',
-      insertTypesEntry: true, // Generates a `types` entry in `package.json`
-    }),
-  ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/main.ts"),
-      name: "alert-dialog",
-      fileName: (format) => `alert-dialog.${format}.js`,
-      formats: ["es", "umd"], 
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
-  },
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
